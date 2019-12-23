e1a9832fbea2f7803fde1f37311d612f49dcf568
skiwi <frank_skiwi@hotmail.com>
2019-12-23 16:55:06 +0100

    Finished day 1a.

--- AdventOfCode2019/Day1a.cs
+++ AdventOfCode2019/Day1a.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
+            var total = File.ReadAllLines("day1a.txt")
+                .Select(line => int.Parse(line))
+                .Select(x => x / 3)
+                .Select(x => x - 2)
+                .Sum();
+            Console.WriteLine(total);
         }
     }
 }
